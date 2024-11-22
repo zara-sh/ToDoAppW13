499fc3558c1a577f448c73a25d6409025ff516b6
JustinP <justin6730@gmail.com>
2024-11-22 14:12:44 +1100

    Implemented CreateTask onAddContact will need to be renamed later tasks gets printed as long while we add things

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import './App.css';
+import CreateTask from './components/CreateTask';
+import { useState, useEffect } from 'react';
 
 /**
  * Using App like main index
  * place holders while we add the componetents
  */
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  /**
+   * Save tasks to localStorage whenever they change
+   * only printing whole task list while testing
+   */
+  useEffect(() => {
+    // localStorage.setItem("contacts", JSON.stringify(contacts));
+    console.log(tasks);
+  }, [tasks]);
+
+  // Add a new task
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <>
       <header>
         <h1>Temp Heading</h1>
       </header>
       <main>
-        <button>Temp Add Button +</button>
-        <p>Temp Place for tasks</p>
+        <CreateTask onAddContact={addTask} />
       </main>
       <footer>
         <p>Temp Footer with credits and such</p>
